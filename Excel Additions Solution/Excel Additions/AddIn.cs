c3d2c92f1e245ca16df4701618918bc4d432a935
Mark Rhoades-Brown <mark@rhoades-brown.co.uk>
2018-07-01 16:48:44 +0100

    Initial commit.

---
+++ Excel Additions Solution/Excel Additions/AddIn.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using ExcelDna.Integration;
+using ExcelDna.Registration;
+
+namespace Excel_Additions
+{
+    public class AddIn : IExcelAddIn
+    {
+        public void AutoOpen()
+        {
+            RegisterFunctions();
+        }
+
+        public void AutoClose()
+        {
+        }
+
+        public void RegisterFunctions()
+        {
+            ExcelRegistration.GetExcelFunctions()
+                             .Select(UpdateHelpTopic)
+                             .RegisterFunctions();
+        }
+
+        public ExcelFunctionRegistration UpdateHelpTopic(ExcelFunctionRegistration funcReg)
+        {
+            funcReg.FunctionAttribute.HelpTopic = "http://www.bing.com";
+            return funcReg;
+        }
+    }
+
+    public class Functions
+    {
+        [ExcelFunction(HelpTopic = "http://www.google.com")]
+        public static object SayHello()
+        {
+            return "Hello!!!";
+        }
+    }
+}
---
+++ Excel Additions Solution/Excel Additions/AdditionalFunctions.cs
@@ -0,0 +1,31 @@
+﻿using ExcelDna.Integration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel_Additions
+{
+    public static class AdditionalFunctions
+    {
+        [ExcelFunction(
+            ExplicitRegistration = true,
+            Description = "My first .NET function"
+        )]
+        public static string JOIN(
+            [ExcelArgument(Name = "Delimiter", Description = "The delimiter between the objects")]string delimiter,            
+            [ExcelArgument(Name = "Value", Description = "gives the Rest")]params object[] args
+            )
+        {
+            var arrayResult = new System.Collections.ArrayList();
+            foreach (var arg in args)
+            {
+                if (!(arg is ExcelEmpty)) {
+                    arrayResult.Add(arg.ToString());
+                }
+            }
+            return String.Join(delimiter, arrayResult.ToArray());
+        }
+    }
+}
