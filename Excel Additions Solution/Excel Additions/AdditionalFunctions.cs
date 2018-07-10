62ef222c27687f8f9dc31bd6690327d1fe7d57d5
Mark Rhoades-Brown <mark@rhoades-brown.co.uk>
2018-07-10 13:57:40 +0100

    Updated function to take an array of parameters
    - Created a couple of new internal functions used by this one to manage the variables better.

--- Excel Additions Solution/Excel Additions/AdditionalFunctions.cs
+++ Excel Additions Solution/Excel Additions/AdditionalFunctions.cs
@@ -9,23 +9,55 @@ namespace Excel_Additions
 {
     public static class AdditionalFunctions
     {
-        [ExcelFunction(
-            ExplicitRegistration = true,
-            Description = "My first .NET function"
-        )]
-        public static string JOIN(
-            [ExcelArgument(Name = "Delimiter", Description = "The delimiter between the objects")]string delimiter,            
-            [ExcelArgument(Name = "Value", Description = "gives the Rest")]params object[] args
-            )
+        #region Helper Functions
+        private static void ProcessItem(System.Collections.ArrayList resultsArray, object item)
         {
-            var arrayResult = new System.Collections.ArrayList();
-            foreach (var arg in args)
+            switch (item.GetType().ToString())
             {
-                if (!(arg is ExcelEmpty)) {
-                    arrayResult.Add(arg.ToString());
+                case "System.Object[,]":
+                    ProcessArray(resultsArray, (Object[,])item);
+                    break;
+                case "System.Object[]":
+                case "System.Array":
+                    ProcessItem(resultsArray, item);
+                    break;
+                case "System.String":
+                    resultsArray.Add(item);
+                    break;
+                default:
+                    resultsArray.Add(item.ToString());
+                    break;
+            }
+        }
+
+        static void ProcessArray(System.Collections.ArrayList resultsArray, object[,] arrayObject)
+        {
+            for (int i = 0; i < arrayObject.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrayObject.GetLength(1); j++)
+                {
+                    ProcessItem(resultsArray, arrayObject[i, j]);
                 }
             }
-            return String.Join(delimiter, arrayResult.ToArray());
         }
+        #endregion
+
+        [ExcelFunction(
+            Description = "Joins a list of strings together, similar to concatenate, but with a seperator",
+            Category = "Text"
+        )]
+        public static string JoinStrings(
+            [ExcelArgument(Name = "seperator", Description = "is the seperator to join with")]string separator,
+            [ExcelArgument(Name = "values", Description = "are values or selections to be joined")]params object[] values)
+        {
+            var resultsArray = new System.Collections.ArrayList();
+            foreach (var value in values)
+            {
+                ProcessItem(resultsArray, value);
+            }
+
+            return String.Join(separator, resultsArray.ToArray());
+        }
+    }
     }
 }
